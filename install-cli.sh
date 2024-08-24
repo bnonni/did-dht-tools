d0261a4439af54dd38c96aa829cd2cfa6d5e435d
Bnonni <bnonni@formfree.com>
2024-08-24 15:26:01 -0400

    update install sh

--- install-cli.sh
+++ install-cli.sh
@@ -1,18 +1,23 @@
-#!/usr/bin/env
+echo "$SHELL detected"
+RC_FILE="$([ "$SHELL" = "/bin/bash" ] && echo "$HOME/.bashrc" || [ "$SHELL" = "/bin/zsh" ] && echo "$HOME/.zshrc")"
+echo "$RC_FILE identified"
+INSTALLED="$(cat $RC_FILE | grep 'alias tool5' | wc -l | awk '{$1=$1;print}')"
 
-set -e
+if [[ "$INSTALLED" -gt 0 ]]; then
+    echo "tool5 already installed"
+    echo "run 'tool5 --help' to see help menu"
+    exit 0
+fi
 
-if [ "$SHELL" == "/bin/bash" ]; then
-    echo "Bash detected. Installing tool5 cli."
-    echo $'\n'"alias tool5="node $PWD/dist/web5-tools.js" >> ~/.bashrc
-    source ~/.bashrc
-elif [ "$SHELL" == "/bin/zsh" ]; then
-    echo "Zsh detected. Installing tool5 cli."
-    echo $'\n'"alias tool5="node $PWD/dist/web5-tools.js" >> ~/.zshrc
-    source ~/.zshrc
-else
-    echo "Unsupported shell!"
+if [[ "$SHELL" =~ ^(bash|zsh)$ ]]; then
+    echo "Unsupported shell"
     exit 1
 fi
 
-echo "tool5 cli successfully installed, run 'tool5 --help' to start using it."
+
+echo "Installing tool5 to $RC_FILE"
+echo $'\n'"alias tool5=\"node $PWD/dist/web5-tools.js\"" >> "$RC_FILE"
+source "$RC_FILE"
+
+echo "tool5 cli successfully installed"
+echo "run 'tool5 --help' to see help menu"
