293b16d2caef72ccced91a9563bb5e6638e8d21c
Bnonni <bnonni@formfree.com>
2024-08-24 14:52:52 -0400

    remove paths install-cli

--- install-cli.sh
+++ install-cli.sh
@@ -3,16 +3,16 @@
 set -e
 
 if [ "$SHELL" == "/bin/bash" ]; then
-    echo "Bash detected. Installing web5-tools cli."
-    echo $'\n''alias web5-tools="node /Users/bnonni/Projects/Personal/web5-tools/dist/web5-tools.js"' >> ~/.bashrc
+    echo "Bash detected. Installing tool5 cli."
+    echo $'\n'"alias tool5="node $PWD/dist/web5-tools.js" >> ~/.bashrc
     source ~/.bashrc
 elif [ "$SHELL" == "/bin/zsh" ]; then
-    echo "Zsh detected. Installing web5-tools cli."
-    echo $'\n''alias web5-tools="node /Users/bnonni/Projects/Personal/web5-tools/dist/web5-tools.js"' >> ~/.zshrc
+    echo "Zsh detected. Installing tool5 cli."
+    echo $'\n'"alias tool5="node $PWD/dist/web5-tools.js" >> ~/.zshrc
     source ~/.zshrc
 else
     echo "Unsupported shell!"
     exit 1
 fi
 
-echo "web5-tools cli successfully installed, run 'web5-tools' to start using it."
+echo "tool5 cli successfully installed, run 'tool5 --help' to start using it."
